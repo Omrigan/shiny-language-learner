60654e22643575dbaaf0fa05a527b3872f268ba4
Omrigan <omrigann@gmail.com>
2016-03-15 16:20:42 +0300

    Interval repeatings

--- study_settings.py
+++ study_settings.py
@@ -1,7 +1,15 @@
 import datetime
 stages = {
     1: datetime.timedelta(hours=0),
-    2: datetime.timedelta(hours=8),
-    3: datetime.timedelta(hours=24),
-    4: datetime.timedelta(weeks=100)
+    2: datetime.timedelta(minutes=30),
+    3: datetime.timedelta(hours=8),
+    4: datetime.timedelta(hours=24),
+    5: datetime.timedelta(weeks=1),
+    6: datetime.timedelta(weeks=4),
+    7: datetime.timedelta(weeks=12),
+    8: datetime.timedelta(weeks=12),
+    9: datetime.timedelta(weeks=12),
+    10: datetime.timedelta(weeks=12),
+    11: datetime.timedelta(weeks=12),
+
 }
