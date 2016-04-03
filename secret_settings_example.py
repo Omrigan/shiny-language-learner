e2e6eb62e9f3df6a76531474b879cbdb50c274ad
Omrigan <omrigann@gmail.com>
2016-04-03 14:55:00 +0300

    Readme and docs

---
+++ secret_settings_example.py
@@ -0,0 +1,9 @@
+import os
+env = os.getenv('BOT_ENV', 'staging')
+bot = {'token' : 'your token'}
+mongo = {'uri': 'mongo uri'}
+translate_yandex = {
+    'token': 'your token for yandex.translate'
+}
+
+
