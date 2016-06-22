526bd8097aa73ebb7cc8ecebcfd51f36dcaa9926
Omrigan <omrigann@gmail.com>
2016-06-22 13:04:04 +0300

    New scheduler

--- remainder.py
+++ remainder.py
@@ -1,60 +1,59 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-
-from apscheduler.jobstores.mongodb import MongoDBJobStore
 from pytz import utc
 from pymongo import MongoClient
 from . import telegram
 import time
 import logging
 import datetime
+import schedule
+import time, threading, random
 
-executors = {
-    'default': ThreadPoolExecutor(20),
-    'processpool': ProcessPoolExecutor(5)
-}
-job_defaults = {
-    'coalesce': False,
-    'max_instances': 3
-}
+jobs = {}
+
+def get_job(chat_id):
+    def send_remainder():
+            telegram.send_message(chat_id, "Hey! It is time to learn")
+            logging.debug("Added job succeed. Chat id: %s" % (chat_id,))
+    jobs[chat_id] = send_remainder
+    return jobs[chat_id]
 
 
 def configure(settings):
-    global remainders, scheduler
+    global remainders
     remainders = MongoClient(settings.mongo['uri']).get_default_database().remainders
 
-    scheduler = BackgroundScheduler(executors=executors, job_defaults=job_defaults,
-                                    timezone=utc)
-    scheduler.start()
+def to_ugly_time(time):
+    return "%s:%s" %(time.hour, time.minute)
+
 
 
 def recover_jobs():
     for r in remainders.find():
         r['time_utc'] += datetime.timedelta(days=(datetime.datetime.utcnow() - r['time_utc']).days)
         remainders.save(r)
-
-        def func():
-            nonlocal r
-            telegram.send_message(r['chat_id'], "Hey! It is time to learn")
-            logging.debug("Recovered job succeed. Chat id: %s" % (r['chat_id']))
-
-        scheduler.add_job(func, 'interval', days=1, next_run_time=r['time_utc'], id='%s_1' % (r['chat_id'],))
+        jobs[r['chat_id']] = schedule.every().day.at(to_ugly_time(r['time_utc'])).do(get_job(r['chat_id']))
 
 
 def add_job(user, time_utc):
-    def func():
-        nonlocal user
-        telegram.send_message(user['chat_id'], "Hey! It is time to learn")
-        logging.debug("Added job succeed. Chat id: %s" % (user['chat_id']))
-
+    jobs[user['chat_id']] = schedule.every().day.at(to_ugly_time(user['time_utc'])).do(get_job(user['chat_id']))
     remainders.save({'chat_id': user['chat_id'],
                      'time_utc': time_utc
                      })
-    scheduler.add_job(func, 'interval', days=1, next_run_time=time_utc, id='%s_1' % (user['chat_id'],))
-    logging.debug(scheduler.get_jobs())
-
 
 def remove_job(user):
-    if scheduler.get_job(job_id='%s_1' % (user['chat_id'],)) is not None:
-        scheduler.remove_job(job_id='%s_1' % (user['chat_id'],))
+    if user['chat_id'] in jobs:
+        schedule.cancel_job(jobs[user['chat_id']])
     remainders.remove({'chat_id': user['chat_id']})
+
+cease_continuous_run = threading.Event()
+
+class ScheduleThread(threading.Thread):
+    @classmethod
+    def run(cls):
+        while not cease_continuous_run.is_set():
+            if random.random()<0.001:
+                logging.warning("Random job queue check - total jobs: %s" % len(schedule.default_scheduler.jobs))
+            schedule.default_scheduler.run_pending()
+            time.sleep(1)
+
+continuous_thread = ScheduleThread()
+continuous_thread.start()
